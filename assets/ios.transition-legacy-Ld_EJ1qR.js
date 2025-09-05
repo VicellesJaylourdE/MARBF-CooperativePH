8ec98d282ba2825e309e2a6d7a1f0e22ace7c62a
VicellesJaylourdE <jayvicelles2003@gmail.com>
2025-09-05 14:17:11 +0800

    Updates

--- assets/ios.transition-legacy-Ld_EJ1qR.js
+++ assets/ios.transition-legacy-Ld_EJ1qR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-0WmGyr0J.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CSZl2Gym.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-lxmXmxEO.js
+++ assets/swipe-back-legacy-lxmXmxEO.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-0WmGyr0J.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CSZl2Gym.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
