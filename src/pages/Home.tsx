8aa74f1fb27c48750d94b0dafd359bd17cb69ef6
VicellesJaylourdE <jayvicelles2003@gmail.com>
2025-09-05 12:55:32 +0800

    added some home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,56 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { 
+  IonButton,
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonIcon, 
+      IonLabel, 
+      IonMenuButton, 
+      IonPage, 
+      IonRouterOutlet, 
+      IonTabBar, 
+      IonTabButton, 
+      IonTabs, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { bookOutline, person, search, star } from 'ionicons/icons';
+import { Route, Redirect } from 'react-router';
+import SignUp from './Register';
+  const Home: React.FC = () => {
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
-};
+    const tabs = [
+      {name:'Feed', tab:'feed',url: '/MARBF-Cooperative01/app/home/feed', icon: bookOutline},
+      {name:'Favorites',tab:'favorites', url: '/MARBF-Cooperative01/app/home/favorites', icon: star},
+      {name:'About me',tab:'About Me', url: '/MARBF-Cooperative01/app/home/about_me', icon: person},
+    ]
+    
+    return (
+      <IonReactRouter>
+        <IonTabs>
+          <IonTabBar slot="bottom">
 
-export default Home;
+            {tabs.map((item, index) => (
+              <IonTabButton key={index} tab={item.tab} href={item.url}>
+                <IonIcon icon={item.icon} />
+                <IonLabel>{item.name}</IonLabel>
+              </IonTabButton>
+            ))}
+            
+          </IonTabBar>
+        <IonRouterOutlet>
+
+        
+
+          <Route exact path="/MARBF-Cooperative01/app/home">
+            <Redirect to="/MARBF-Cooperative01/app/home/feed" />
+          </Route>
+
+        </IonRouterOutlet>
+        </IonTabs>
+      </IonReactRouter>
+    );
+  };
+  
+  export default Home;
