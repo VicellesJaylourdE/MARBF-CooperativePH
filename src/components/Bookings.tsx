46311fb80d013d63d3b21e0068131c31d8302411
VicellesJaylourdE <jayvicelles2003@gmail.com>
2025-09-14 19:31:56 +0800

    added bookings

---
+++ src/components/Bookings.tsx
@@ -0,0 +1,10 @@
+const Bookings: React.FC = () => {
+  return (
+    <div className="equipment-section">
+      <h2 className="equipment-title">My Bookings</h2>
+      <p className="equipment-sub">View and manage your bookings here.</p>
+    </div>
+  );
+};
+
+export default Bookings;
