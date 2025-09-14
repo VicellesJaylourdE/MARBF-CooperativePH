ddd81f3cd785c3cdf8279d26f2bfb5e552e24afb
VicellesJaylourdE <jayvicelles2003@gmail.com>
2025-09-14 21:28:06 +0800

    Updates

--- assets/md.transition-legacy-BeL-wI1d.js
+++ assets/md.transition-legacy-BeL-wI1d.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C3CtFqfu.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-0-Oz8V67.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
