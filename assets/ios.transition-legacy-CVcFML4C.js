1fac42ae5e3ad58e3647ec517c4073a8531e20b8
VicellesJaylourdE <jayvicelles2003@gmail.com>
2025-09-17 23:12:57 +0800

    Updates

--- assets/ios.transition-legacy-CVcFML4C.js
+++ assets/ios.transition-legacy-CVcFML4C.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D6X-WHHD.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DFF2-mAa.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
