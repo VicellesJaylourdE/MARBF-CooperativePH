3e373e8756528a5dab2698a712cf901830533af4
VicellesJaylourdE <jayvicelles2003@gmail.com>
2025-09-25 20:45:19 +0800

    Updates

--- assets/md.transition-legacy-g7R2psfB.js
+++ assets/md.transition-legacy-g7R2psfB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D5cRQ53Q.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-4C5s7Kl4.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
