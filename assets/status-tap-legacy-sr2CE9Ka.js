3faf1fe385c700311d8a8897f0779bf97283244c
VicellesJaylourdE <jayvicelles2003@gmail.com>
2025-09-30 09:12:13 +0800

    Updates

--- assets/status-tap-legacy-sr2CE9Ka.js
+++ assets/status-tap-legacy-sr2CE9Ka.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CFyqhROC.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-teH3LYSG.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
