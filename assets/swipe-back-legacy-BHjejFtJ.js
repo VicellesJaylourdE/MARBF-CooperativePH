04265b1a85c53bc82725473b906b3bad75222c99
VicellesJaylourdE <jayvicelles2003@gmail.com>
2025-10-01 11:18:16 +0800

    Updates

--- assets/swipe-back-legacy-BHjejFtJ.js
+++ assets/swipe-back-legacy-BHjejFtJ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-teH3LYSG.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CQAM8ME4.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
