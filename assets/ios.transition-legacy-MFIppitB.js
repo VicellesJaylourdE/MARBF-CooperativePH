8fdce1cc09a3062c8a4c8fd1c83640c3de77cda5
VicellesJaylourdE <jayvicelles2003@gmail.com>
2025-10-12 13:20:33 +0800

    Updates

--- assets/ios.transition-legacy-MFIppitB.js
+++ assets/ios.transition-legacy-MFIppitB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-7v5EN9k-.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-B8_eGnhg.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
