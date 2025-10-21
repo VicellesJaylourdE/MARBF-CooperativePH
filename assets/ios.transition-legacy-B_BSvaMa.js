2e694409c4f167c36f56043323ae941fbaf8ec8b
VicellesJaylourdE <jayvicelles2003@gmail.com>
2025-10-21 08:34:45 +0800

    Updates

--- assets/ios.transition-legacy-B_BSvaMa.js
+++ assets/ios.transition-legacy-B_BSvaMa.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DkDC7kKY.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DlS4mNp7.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
